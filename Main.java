6882a388720667644dffa356a6a6c6a723862c64
Nour Tarek <nourtarrek.2@gmail.com>
2023-10-27 21:55:02 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,9 +1,9 @@
 import java.io.*;  // Import the File class
 import java.util.Scanner;
 
-//remember to handle exceptions errors like delete then list....
+//remember to handle exceptions errors 
 public class Main{
-  public static void main(String[] args) /*throws IOException*/{
+  public static void main(String[] args) {
     Parser p=new Parser();
     p.parse("rm file1     file2.txt ");
     System.out.println(p.getCommandName());
