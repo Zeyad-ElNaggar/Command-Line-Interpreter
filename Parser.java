86b6c00cac71e75a09960bce27720782d4762189
Nour Tarek <nourtarrek.2@gmail.com>
2023-10-30 00:02:30 +0200

    Add files via upload

--- Parser.java
+++ Parser.java
@@ -4,14 +4,15 @@ public class Parser{
     
     //constructor
     public Parser(){
-        args=new String[10];
     }
 
     //This method will divide the input into commandName and args
     //where "input" is the string command entered by the user
     public void parse(String input){
         input=input.trim();
-        commandName= input.split(" ")[0];
+        this.commandName= input.split(" ")[0];
+        args=new String[input.split(" ").length-1];
+        
         for(int i=1;i<input.split(" ").length;i++){
             if(input.split(" ")[i].length()>0 && input.split(" ")[i].charAt(0)!=' ')
                 args[i-1]=input.split(" ")[i];
