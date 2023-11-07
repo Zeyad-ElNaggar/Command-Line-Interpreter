217a86771d536e8885979b0ab256bfae8321f5e4
Maram Wael <100848741+maramwael188@users.noreply.github.com>
2023-11-07 23:18:24 +0200

    nshel Maram

--- Main.java
+++ Main.java
@@ -4,7 +4,6 @@ import java.util.Scanner;
 //remember to handle exceptions errors like delete then list....
 public class Main{
   public static void main(String[] args) /*throws IOException*/{
-    System.out.println("Maram's Terminal");
     Terminal terminal=  new Terminal();
     terminal.chooseCommandAction();
 
