9578ebb34938ed828f478aade2d58c059ce746e1
Zeyad-ElNaggar <100488223+Zeyad-ElNaggar@users.noreply.github.com>
2023-11-07 23:13:04 +0200

    Update

--- Parser.java
+++ Parser.java
@@ -1,18 +1,21 @@
-public class Parser{
+//parser class
+class Parser{
     String commandName;
     String[] args;
-    
+
     //constructor
     public Parser(){
     }
 
     //This method will divide the input into commandName and args
     //where "input" is the string command entered by the user
+
+
     public void parse(String input){
         input=input.trim();
         this.commandName= input.split(" ")[0];
         args=new String[input.split(" ").length-1];
-        
+
         for(int i=1;i<input.split(" ").length;i++){
             if(input.split(" ")[i].length()>0 && input.split(" ")[i].charAt(0)!=' ')
                 args[i-1]=input.split(" ")[i];
@@ -26,4 +29,5 @@ public class Parser{
     public String[] getArgs(){
         return this.args;
     }
+
 }
--- Terminal.java
+++ Terminal.java
@@ -1,14 +1,13 @@
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
-import java.util.Scanner;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
-public class Terminal {
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class Terminal {
     Parser parser;
     Path myPath;
 
@@ -21,63 +20,59 @@ public class Terminal {
     public void cd(String[] args) {
         if (args.length == 0) {   //changes the current path to the path of your home directory
             myPath = Paths.get(System.getProperty("user.dir"));
-        }
-        else {
-            String path = "";
-            for (int i = 0; i < args.length - 1; i++) {
-                path += args[i] + " ";
-            }
-            path += args[args.length - 1];
+        } else {
 
-            if (path.equals("..")) {   ///changes the current directory to the previous directory.
+            if (args[0].equals("..")) {   ///changes the current directory to the previous directory.
                 this.myPath = myPath.getParent();
             } else {
-                Boolean flag = false;       //to check if the
-                Path newPath = myPath.resolve(Paths.get(path));  //
-                File file = new File(newPath.toString());// folder exist in the directory or not
-                if (file.isDirectory()) {
-                    myPath = myPath.resolve(newPath);
-                    flag=true;
+                String path = "";
+                for (int i = 0; i < args.length - 1; i++) {
+                    path += args[i] + " ";
                 }
-                if (flag == false)
-                    System.out.println(path + " does not exist in the current directory");
+                path += args[args.length - 1];
+
+                Path newPath = myPath.resolve(Paths.get(path));
+                File file = new File(newPath.toString());
+
+                if (!file.exists()) {
+                    System.out.println("path does not exist in the current directory");
+                } else {
+                    myPath = newPath;
+                }
             }
         }
     }
-    //lists folder's content in order
-    public void ls(String []args){
-        File file= new File(myPath.toString());
-        String [] listFolders = file.list(); 
 
-        if (listFolders.length==0){   //if folder is empty
+    //lists folder's content in order
+    public void ls(String[] args) {
+        File file = new File(myPath.toString());
+        String[] listFolders = file.list();
+
+        if (listFolders.length == 0) {   //if folder is empty
             System.out.println("The current directory is empty");
-        }
-        else if(args.length==0){
+        } else if (args.length == 0) {
             for (String folder : listFolders) {
                 System.out.println(folder);
             }
-        }
-        else if(args[0].equals("-r")){//lists folder's content in reverse order
-            for (int i=listFolders.length-1;i>=0;i--) {
+        } else if (args[0].equals("-r")) {//lists folder's content in reverse order
+            for (int i = listFolders.length - 1; i >= 0; i--) {
                 System.out.println(listFolders[i]);
             }
-        }
-        else{
+        } else {
             System.out.println("Wrong Command");
         }
-        
+
     }
 
 
     //create folder by giving its name or its name and path
-    public void mkdir(String []args) {
+    public void mkdir(String[] args) {
         if (args.length == 0)
             System.out.println("Wrong input for this command");
         else {
             File file = new File(myPath.toString());
             for (String directoryNamePath : args) {
                 if (directoryNamePath != null && file.isDirectory()) {
-
                     Path newPath = myPath.resolve(Paths.get(directoryNamePath));
                     file = new File(newPath.toString());
                     file.mkdir();
@@ -90,43 +85,43 @@ public class Terminal {
     }
 
     //creates a file by giving its name or path
-    public void touch(String []args){
+    public void touch(String[] args) {
         for (String fileNamePath : args) {
-            if(fileNamePath!=null){
-                try{
-                    Path newPath= myPath.resolve(fileNamePath);
-                    File file=new File(newPath.toString());
+            if (fileNamePath != null) {
+                try {
+                    Path newPath = myPath.resolve(fileNamePath);
+                    File file = new File(newPath.toString());
                     file.createNewFile();
-                    System.out.println( fileNamePath+" is created successfully");
-                }
-                catch(Exception fileNotCreated){
+                    System.out.println(fileNamePath + " is created successfully");
+                } catch (Exception fileNotCreated) {
                     System.out.println("Error file couldn't be created..try again\n");
                 }
             }
         }
-        
+
     }
 
     //Implement each command in a method, for example:
-    public void pwd(){
+    public void pwd() {
         System.out.println(myPath.toString());
     }
 
     //echo method takes argument and prints it
-    public void echo(String []args){
+    public void echo(String[] args) {
         for (String argument : args) {
             System.out.println(argument);
         }
     }
 
     //delete file
-    public void rm(String []args) {
+    public void rm(String[] args) {
         if (args.length != 1) {
             System.out.println("Wrong input for this command");
-        }
-        else {
-            String fileName=args[0];
-            File file = new File(myPath.resolve(fileName).toString());
+        } else {
+            String fileName = args[0];
+            Path newPath = myPath.resolve(Paths.get(fileName));
+            File file = new File(newPath.toString());
+
             if (file.isDirectory())
                 System.out.println("rm: cannot remove " + fileName + " : Is a directory");
             else if (file.isFile()) {
@@ -136,19 +131,18 @@ public class Terminal {
                 System.out.println("there is no " + fileName + " in the current directory");
         }
     }
+
     //Removes each given directory only if it is empty
-    public void rmdir(String [] args) {
-        if (args.length == 0 || args.length>1) {
+    public void rmdir(String[] args) {
+        if (args.length == 0 || args.length > 1) {
             System.out.println("Wrong input for this command");
-        }
-        else if (args[0].equals(String.valueOf('*'))) {  //'*' -> delete all  empty folders
+        } else if (args[0].equals(String.valueOf('*'))) {  //'*' -> delete all  empty folders
             File originalFile = new File(myPath.toString());
             String[] listFolders = originalFile.list();
 
-            if(listFolders.length==0){   //checking if the folder is already empty
+            if (listFolders.length == 0) {   //checking if the folder is already empty
                 System.out.println("The current directory is empty");
-            }
-            else {       //if the folder is not empty
+            } else {       //if the folder is not empty
                 Boolean flag = false; //to check if there is "at least" an empty folder in the directory
 
                 for (String folder : listFolders) {               //searching for every folder in the main path
@@ -185,67 +179,62 @@ public class Terminal {
     }
 
     //cat command 
-     public void cat(String []args) {
+    public void cat(String[] args) {
         File file = new File(args[0]);
 
         //Handling if user doesn't input .txt in file name
-        if(!file.isFile()){
-            args[0]+=".txt";
+        if (!file.isFile()) {
+            args[0] += ".txt";
             file = new File(args[0]);
         }
-        if(args.length==2 ){
-            File file2= new File(args[1]);
-            if(!file2.isFile()){
-                args[1]+=".txt";
-                file2= new File(args[1]);
+        if (args.length == 2) {
+            File file2 = new File(args[1]);
+            if (!file2.isFile()) {
+                args[1] += ".txt";
+                file2 = new File(args[1]);
             }
         }
-        
+
         if (args.length == 1 && file.isFile()) {
-            try{
+            try {
                 BufferedReader reader = new BufferedReader(new FileReader(args[0]));
                 String line;
                 while ((line = reader.readLine()) != null) {
                     System.out.println(line);
                 }
-            } 
-            catch (IOException e) {
+            } catch (IOException e) {
                 System.err.println("File not found");
             }
-        }
-        else if(args.length==2 && file.isFile()){
+        } else if (args.length == 2 && file.isFile()) {
             try {
-                BufferedReader reader=new BufferedReader(new FileReader(args[0]));
-                FileWriter myWriter=new FileWriter(args[1], true); //append mode on
+                BufferedReader reader = new BufferedReader(new FileReader(args[0]));
+                FileWriter myWriter = new FileWriter(args[1], true); //append mode on
                 String line;
                 myWriter.write(" ");
-                while ((line= reader.readLine()) !=null) {
-                        myWriter.write(line);
-                        System.out.println("Successfully wrote to the file.");
+                while ((line = reader.readLine()) != null) {
+                    myWriter.write(line);
+                    System.out.println("Successfully wrote to the file.");
                 }
                 myWriter.close();
-            } 
-            catch (IOException e) {
+            } catch (IOException e) {
                 System.err.println("File not found");
             }
 
-        }
-            
-        else{
-            
+        } else {
+
             System.out.println("wrong input");
         }
     }
 
     //word count
-    public void wc(String []args){
+    public void wc(String[] args) {
         File file = new File(args[0]);
-        if(!file.isFile()){
-            args[0]+=".txt";
+        if (!file.isFile()) {
+            args[0] += ".txt";
             file = new File(args[0]);
         }
-        if(file.isFile()){
-            int lineNum = 0, wordNum= 0, charNum = 0;
+        if (file.isFile()) {
+            int lineNum = 0, wordNum = 0, charNum = 0;
             try {
                 BufferedReader reader = new BufferedReader(new FileReader(args[0]));
                 String line;
@@ -257,164 +246,116 @@ public class Terminal {
                         charNum += word.length();
                     }
                 }
-                System.out.println("Number of lines: " + lineNum +", " +"Number of words: " + wordNum + ", " + "Number of characters: " + charNum + ", " + args[0]);
-            } 
-            catch (IOException e) {
+                System.out.println("Number of lines: " + lineNum + ", " + "Number of words: " + wordNum + ", " + "Number of characters: " + charNum + ", " + args[0]);
+            } catch (IOException e) {
                 System.err.println("File not found");
             }
-        }
-
-        else{
+        } else {
             System.out.println("Please enter a text file name");
         }
     }
+
     //cp cmnd
-    public void cp(String srcFile, String dstFile) {
-        try {
-            Files.copy(Paths.get(srcFile), Paths.get(dstFile), StandardCopyOption.REPLACE_EXISTING);
-            System.out.println("Copied successfully.");
-        } catch (IOException e) {
-            System.out.println("Error while copying." + e.getMessage());
-            e.printStackTrace();
-        }
-    }
-//cpr cmnd
-    public void cpr(String srcDirc, String dstDircٍٍ) {
-        if (parser.getArgs()[0]== null) {
-            System.out.println("Src is Null");
-            return;
-        }
-        if (parser.getArgs()[1]== null) {
-            System.out.println("Dst is Null");
-            return;
-        }
-        try {
-            File srcDir = new File(srcDirc);
-            File destDir = new File(dstDircٍٍ);
+    public void cp(String[] args) {
 
-            if (srcDir == null) {
-                System.out.println("Src is Null");
-                return;
-            }
+        if (args.length < 2 || args.length > 2) {
+            System.out.println("wrong input");
+        } else {
+            Path f1Path = myPath.resolve(Paths.get(args[0]));
+            Path f2Path = myPath.resolve(Paths.get(args[1]));
 
-            if (destDir == null) {
-                System.out.println("Dst is Null");
-                return;
-            }
 
-            if (!srcDir.exists() || !srcDir.isDirectory()) {
-                System.out.println("Source directory does not exist or is not a directory");
-                return;
-            }
+            File f1 = new File(f1Path.toString());
+            File f2 = new File(f2Path.toString());
 
-            if (!destDir.exists()) {
-                // Create the destination directory mkdir function
-                mkdir(new String[]{dstDircٍٍ});
-            }
-
-            if (!destDir.isDirectory()) {
-                System.out.println("Dest is not a directory");
-                return;
-            }
-
-            File[] sourceFiles = srcDir.listFiles();
-
-            if (sourceFiles != null) {
-                for (File source : sourceFiles) {
-                    if (source != null) {
-                        File newDestination = new File(destDir, source.getName());
-
-                        if (newDestination.exists()) {
-                            System.out.println("Dst exists! " + newDestination);
-                        } else {
-                            if (source.isDirectory()) {
-                                mkdir(new String[]{newDestination.getPath()});
-                                cpr(source.getPath(), newDestination.getPath()); // Recursively copy the subdirectory
-                            } else {
-                                try {
-                                    Files.copy(source.toPath(), newDestination.toPath(), StandardCopyOption.REPLACE_EXISTING);
-                                } catch (IOException e) {
-                                    e.printStackTrace();
-                                    System.out.println("Copying failed");
-                                }
-                            }
-                        }
-                    } else {
-                        System.out.println("Src = null.");
-                    }
+            if (args.length == 2 && f1.isFile() && f2.isFile()) {
+                try {
+                    Files.copy(f1Path, f2Path, StandardCopyOption.REPLACE_EXISTING);
+                    System.out.println("Copied successfully");
+                } catch (IOException e) {
+                    System.out.println("Error while copying." + e.getMessage());
                 }
+            } else {
+                System.out.println("wrong input");
             }
-
-            System.out.println("Copied successfully");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Copying failed");
         }
     }
 
-
-
-    public void exit(){
-        System. exit(0);
+    public void history(List<String> l) {
+        int cnt = 0;
+        for (String str : l) {
+            cnt++;
+            System.out.print(cnt + " ");
+            System.out.println(str);
+        }
     }
+
+    public void exit() {
+        System.exit(0);
+    }
+
     // ...
     //This method will choose the suitable command method to be called
-    public void chooseCommandAction(){
+    public void chooseCommandAction() {
 
-        Scanner scan=new Scanner(System.in);
+        Scanner scan = new Scanner(System.in);
+        List<String> commands = new ArrayList<>();
+
         while (true) {
             System.out.println(myPath);
             System.out.print("> ");
-            String input=scan.nextLine();
+            String input = scan.nextLine();
             parser.parse(input);
-            String command= parser.getCommandName();
-                if(command.equals("echo"))
-                    echo(parser.getArgs());
+            String command = parser.getCommandName();
+            boolean flag = true;
+            if (command.equals("echo"))
+                echo(parser.getArgs());
 
-                else if(command.equals("pwd"))
+            else if (command.equals("pwd")) {
+                if (parser.getArgs().length != 0)
+                    System.out.println("Wrong input");
+                else
                     pwd();
+            } else if (command.equals("ls"))
+                ls(parser.getArgs());
 
-                else if(command.equals("ls"))
-                    ls(parser.getArgs());
+            else if (command.equals("cd"))
+                cd(parser.getArgs());
 
-                else if(command.equals("cd"))
-                    cd(parser.getArgs());
+            else if (command.equals("mkdir"))
+                mkdir(parser.getArgs());
 
-                else if(command.equals("mkdir"))
-                    mkdir(parser.getArgs());
+            else if (command.equals("touch"))
+                touch(parser.getArgs());
 
-                else if(command.equals("touch"))
-                    touch(parser.getArgs());
+            else if (command.equals("rm"))
+                rm(parser.getArgs());
 
-                else if(command.equals("rm"))
-                    rm(parser.getArgs());
+            else if (command.equals("rmdir"))
+                rmdir(parser.getArgs());
 
-                else if(command.equals("rmdir"))
-                    rmdir(parser.getArgs());
+            else if (command.equals("cat"))
+                cat(parser.getArgs());
 
-                else if(command.equals("cat"))
-                    cat(parser.getArgs());
+            else if (command.equals("wc"))
+                wc(parser.getArgs());
 
-                else if(command.equals("wc"))
-                    wc(parser.getArgs());
+            else if (command.equals("exit"))
+                exit();
 
-                else if(command.equals("exit"))
-                    exit();
+            else if (command.equals("history"))
+                history(commands);
 
-                else if (command.equals("cp")) {
-                    if (parser.getArgs().length > 2 && parser.getArgs()[0].equals("-r")) {
-                        String sourcePath = parser.getArgs()[1];
-                        String destinationPath = parser.getArgs()[2];
-                        cpr(sourcePath, destinationPath);
-                    } else if (parser.getArgs().length == 2) {
-                        cp(parser.getArgs()[0], parser.getArgs()[1]);
-                    } else {
-                        System.out.println("Invalid 'cp' or 'cp -r' command. Usage: cp <sourceFile> <destinationFile> or cp -r <sourceDirectory> <destinationDirectory>");
-                    }
-                }
-                else{
-                    System.out.println("Command not found");
-                }
-        }   
+            else if (command.equals("cp"))
+                cp(parser.getArgs());
+
+            else {
+                System.out.println("Command not found");
+                flag = false;
+            }
+            if (flag) {
+                commands.add(input);
+            }
+        }
     }
 }
